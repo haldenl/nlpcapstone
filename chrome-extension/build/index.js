22b928b77b9e23969c2da65e44d245b166f15250
haldenl <haldenl@cs.washington.edu>
2018-05-05 17:38:43 -0700

    add chrome-extension package

---
+++ chrome-extension/build/index.js
@@ -0,0 +1,9 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.index = {})));
+}(this, (function (exports) { 'use strict';
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
